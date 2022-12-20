a8d9f46bda8230eace88b1241e298e1284184b81
prajitadhikari <prajit.raj.adhikari@gmail.com>
2022-12-20 23:10:30 +0545

    workign with some animations

--- random.js
+++ random.js
@@ -17,4 +17,16 @@ animatedElement.style.transform = 'translateY(-20px)';
 // Animate the element when the page loads
 window.addEventListener('load', () => {
     animatedElement.style.opacity = 1;
-    animatedElement.style.transform = 'translateY(0)';
+    animatedElement.style.transform = 'translateY(0)';}
+);
+
+
+const element = document.getElementById('animated-element');
+
+element.addEventListener('mouseover', function() {
+    this.style.animation = 'none';
+});
+
+element.addEventListener('mouseout', function() {
+    this.style.animation = 'fadeInOut 3s ease-in-out infinite';
+});
